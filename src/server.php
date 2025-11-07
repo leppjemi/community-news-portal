92da88a5ad981bf284081998e050fea1ef3c8e19
liff <aliffnajmi50@gmail.com>
2025-11-08 05:28:52 +0800

    live v1.2

--- src/server.php
+++ src/server.php
@@ -3,11 +3,9 @@
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 // If the requested file exists in the public directory, let the built-in server serve it directly
-if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
 
 // Otherwise, forward the request to Laravel's front controller
-require_once __DIR__ . '/public/index.php';
-
-
+require_once __DIR__.'/public/index.php';
